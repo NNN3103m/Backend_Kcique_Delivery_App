d8cca200dd289f01c884311bf5abb6b550a41ae7
nnn31 <nnn3103m@gmail.com>
2021-08-06 01:19:36 -0600

    -Initial commit

---
+++ src/main/kotlin/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/backend/BackendKciqueApplication.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendKciqueApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendKciqueApplication>(*args)
+}
---
+++ src/test/kotlin/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/backend/BackendKciqueApplicationTests.kt
@@ -0,0 +1,13 @@
+package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendKciqueApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

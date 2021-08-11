ae5bbade3e5cdb9ad2e12212d94005d8fbe0668a
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-08-11 17:13:04 -0600

    remove repositories

--- src/main/kotlin/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/backend/repositories/ItemRepository.kt
+++
@@ -1,9 +0,0 @@
-package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.backend.repositories
-
-import hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.backend.models.ItemModel
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-
-@Repository
-interface ItemRepository : JpaRepository<ItemModel, Long>

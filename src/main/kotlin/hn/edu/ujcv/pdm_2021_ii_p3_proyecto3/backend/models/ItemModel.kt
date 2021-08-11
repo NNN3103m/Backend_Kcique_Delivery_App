a7b1361514882b66a22e4b05417c650f3108ce67
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-08-11 14:21:50 -0600

    validations implementation

--- src/main/kotlin/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/backend/models/ItemModel.kt
+++ src/main/kotlin/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/backend/models/ItemModel.kt
@@ -2,6 +2,7 @@ package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3.backend.models
 
 import javax.persistence.Column
 import javax.persistence.Id
+import javax.validation.constraints.Email
 import javax.validation.constraints.Max
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
@@ -9,7 +10,8 @@ import javax.validation.constraints.NotBlank
 class ItemModel {
 
 //    Validations
-    @field:NotBlank
+
+//    @field:NotBlank
 //    @field:Min(1)
 //    @field:Max(150)
 //    val id = Int
@@ -17,19 +19,23 @@ class ItemModel {
 //    val price = Double
 
 
-    @Column(name = "itemId", unique = true, insertable = true, nullable = false, length = 15 )
+    @Column(name = "itemId", unique = true, insertable = true, nullable = false )
     @field:Min(7)
     @field:Max(15)
+    @field:NotBlank
+    @field:Id
     var id: Int? = null
 
     @Column(name = "itemName", unique = false, insertable = true, nullable = false )
     @field:Min(2)
     @field:Max(30)
+    @field:NotBlank
     var name: String? = null
 
     @Column(name = "itemPrice", unique = false, insertable = true, nullable = true )
     @field:Min(1)
     @field:Max(8)
+    @field:NotBlank
     var price: Double? = null
 
     var photoUrl: String? = null
@@ -37,6 +43,7 @@ class ItemModel {
     @Column(name = "itemCategory", unique = false, insertable = true, nullable = false )
     @field:Min(7)
     @field:Max(15)
+    @field:NotBlank
     var category: String? = null
 
     var shopModel: ShopModel
